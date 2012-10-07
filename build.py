f531d36231b370a4a5c5a9919cd7d392c57e9868
Ceasar Bautista <cbautista2010@gmail.com>
2012-10-07 10:58:25 -0400

    Stashing changes
    This branch does everything manually, when really we want to use jinja
    as much as possible

--- build.py
+++ build.py
@@ -8,22 +8,32 @@ import os
 import sys
 
 from jinja2 import Environment, FileSystemLoader
-
-
-# Directory to search for templates
-TEMPLATE_DIR = "./templates"
-
-# Any extensions that should be added to Jinja
-JINJA_EXTENSIONS = []
-
 try:
-    from hamlish_jinja import HamlishExtension
-    JINJA_EXTENSIONS.append(HamlishExtension)
+    import watchdog.events
+    import watchdog.observers
 except ImportError:
     pass
 
+# Any extensions that should be added to Jinja
+JINJA_EXTENSIONS = []
+try:
+    from hamlish_jinja import HamlishExtension
+except ImportError:
+    pass
+else:
+    JINJA_EXTENSIONS.append(HamlishExtension)
 
-def build_template(env, template_name, **kwargs):
+# Absolute path to project
+PROJECT_PATH = os.path.realpath(os.path.dirname(__file__))
+
+# Directory to search for templates
+TEMPLATE_DIR = "templates"
+
+# Absolute path to templates
+TEMPLATE_PATH = os.path.join(PROJECT_PATH, TEMPLATE_DIR)
+
+
+def render_template(env, template_name, **kwargs):
     """Compile a template.
     *   env should be a Jinja environment variable indicating where to find the
         templates.
@@ -32,9 +42,10 @@ def build_template(env, template_name, **kwargs):
     *   kwargs should be a series of key-value pairs. These items will be
         passed to the template to be used as needed.
     """
-    name, ext = os.path.splitext(template_name)
-    print "Building %s..." % template_name
-    template = env.get_template(template_name)
+    relpath = os.path.relpath(template_name, TEMPLATE_PATH)
+    name, ext = os.path.splitext(relpath)
+    print "Building %s..." % relpath
+    template = env.get_template(relpath)
     output_name = '%s.html' % name
     with open(output_name, "w") as f:
         f.write(template.render(**kwargs))
@@ -47,16 +58,6 @@ def parse_csv(filename):
     """
     with open(filename, 'rb') as f:
         return list(csv.DictReader(f))
-
-
-def should_compile(filename):
-    """Check if the file should be compiled.
-    -   Hidden files will not be compiled.
-    -   Files prefixed with an underscore are assumed to be partials and will
-        not be compiled.
-    """
-    # Don't compile partials or hidden files
-    return not (filename.startswith('_') or filename.startswith("."))
 
 
 # Map of filenames to context generators
@@ -72,53 +73,83 @@ def context_generator(filename):
     return wrapper
 
 
-def main():
+def should_render(filename):
+    """Check if the file should be rendered.
+    -   Hidden files will not be rendered.
+    -   Files prefixed with an underscore are assumed to be partials and will
+        not be rendered.
+    -   Directories will not be rendered.
+    """
+    # Don't render partials or hidden files
+    return not (filename.startswith('_')
+        or filename.startswith(".")
+        or (len(filename.split(".")) == 1))
+
+
+def find_templates(searchpath):
+    """Find all templates in a directory."""
+    for root, _, filenames in os.walk(searchpath):
+        for filename in filenames:
+            if should_render(filename):
+                yield os.path.join(root, filename)
+
+
+def render_templates(env, searchpath):
     """Compile each of the templates."""
-    env = Environment(loader=FileSystemLoader(searchpath=TEMPLATE_DIR),
+    for filename in find_templates(searchpath):
+        try:
+            context = CONTEXTS[filename]()
+        except KeyError:
+            context = {}
+        render_template(env, filename, **context)
+
+
+def watch_templates(path, event_handler, poll_frequency=1):
+    """Watch a directory for changes.
+    -   path is the path to monitor
+    -   event_handler is be an event handler which specifies the behavior
+    -   poll_frequency is the frequency (measured in seconds) that we poll at
+    """
+    import time
+
+    # Start watching for any changes
+    observer = watchdog.observers.Observer()
+    observer.schedule(event_handler, path=path, recursive=True)
+    observer.start()
+    try:
+        while True:
+            time.sleep(poll_frequency)
+    except KeyboardInterrupt:
+        observer.stop()
+    observer.join()
+
+
+def main(argv):
+    env = Environment(loader=FileSystemLoader(searchpath=TEMPLATE_PATH),
                       extensions=JINJA_EXTENSIONS)
-    filenames = os.listdir(TEMPLATE_DIR)
-    for filename in filenames:
-        if should_compile(filename):
-            try:
-                context = CONTEXTS[filename]()
-            except KeyError:
-                context = {}
-            build_template(env, filename, **context)
+    render_templates(env, TEMPLATE_PATH)
     print "Templates built."
-    return 0
-
-
-if __name__ == "__main__":
-    main()
-
-    if len(sys.argv) > 1 and sys.argv[1] == '--watch':
-        import time
-
-        from watchdog.observers import Observer
-        from watchdog.events import FileSystemEventHandler
-
-        class JinjaEventHandler(FileSystemEventHandler):
+    if len(argv) > 1 and argv[1] == '--watch':
+        class JinjaEventHandler(watchdog.events.FileSystemEventHandler):
             """
             Naive recompiler.
             Rebuilds all templates if anything changes in /templates.
             """
             def on_modified(self, event):
-                print "Recompiling templates..."
                 super(JinjaEventHandler, self).on_created(event)
-                if event.src_path.endswith("/templates"):
-                    main()
-
-        # Start watching for any changes
+                # This is inefficient, but it's not obvious how to see which
+                # file changed from the event object we get back. Namely,
+                # watchdog seems to only notice .swp files changing, rather
+                # than the src files
+                if event.src_path.startswith(TEMPLATE_PATH):
+                    render_templates(env, TEMPLATE_PATH)
         event_handler = JinjaEventHandler()
-        observer = Observer()
-        observer.schedule(event_handler, path=TEMPLATE_DIR)
-        observer.start()
-        print "Watching '%s' for changes..." % TEMPLATE_DIR
+        print "Watching '%s' for changes..." % TEMPLATE_PATH
         print "Press Ctrl+C to stop."
-        try:
-            while True:
-                time.sleep(1)
-        except KeyboardInterrupt:
-            observer.stop()
+        watch_templates(TEMPLATE_PATH, event_handler)
         print "Process killed"
-        observer.join()
+    return 0
+
+
+if __name__ == "__main__":
+    sys.exit(main(sys.argv))
